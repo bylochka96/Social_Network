b0d6deab014f4dfc55cfecc5731308e1c0a42691
Gutnik Efim <bylochka96@gmail.com>
2021-07-27 16:48:52 +0300

    switch between tabs without reload of the page with help of NavLink

--- src/App.js
+++ src/App.js
@@ -16,8 +16,6 @@ const App = () => {
                 <Header/>
                 <Nav/>
                 <div className="app-wrapper-content">
-                    {/*<Profile/>*/}
-                    {/*<Dialogs/>*/}
                     <Route path='/dialogs' component={Dialogs}/>
                     <Route path='/profile' component={Profile}/>
                     <Route path='/news' component={News}/>
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,14 +1,24 @@
 import style from './Nav.module.css'
+import {NavLink} from "react-router-dom";
 
 const Nav = () => {
     return (
         <nav className={style.nav}>
-            <div className={style.item}><a href='/profile'>Profile</a></div>
-            <div className={style.item}><a href='/dialogs'>Message</a></div>
-            <div className={style.item}><a href='/news'>News</a></div>
-            <div className={style.item}><a href='/music'>Music</a></div>
+            <div className={style.item}>
+                <NavLink to='/profile' activeClassName={style.selectedLink}>Profile</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink to='/dialogs' activeClassName={style.selectedLink}>Message</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink to='/news' activeClassName={style.selectedLink}>News</NavLink></div>
+            <div className={style.item}>
+                <NavLink to='/music' activeClassName={style.selectedLink}>Music</NavLink>
+            </div>
             <br/>
-            <div className={style.item}><a href='/settings'>Settings</a></div>
+            <div className={style.item}>
+                <NavLink to='/settings' activeClassName={style.selectedLink}>Settings</NavLink>
+            </div>
         </nav>
     )
 }
