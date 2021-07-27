74e4bd4cb8e12d6ccc47ed8f6fc55ee21871696e
Gutnik Efim <bylochka96@gmail.com>
2021-07-27 16:22:30 +0300

    use BrowserRouter for switching between tabs

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import './App.css';
+import {BrowserRouter, Route} from "react-router-dom";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 
 const App = () => {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Nav/>
-            <div className="app-wrapper-content">
-                {/*<Profile/>*/}
-                <Dialogs/>
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Nav/>
+                <div className="app-wrapper-content">
+                    {/*<Profile/>*/}
+                    {/*<Dialogs/>*/}
+                    <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/profile' component={Profile}/>
+                    <Route path='/news' component={News}/>
+                    <Route path='/music' component={Music}/>
+                    <Route path='/settings' component={Settings}/>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,12 +3,12 @@ import style from './Nav.module.css'
 const Nav = () => {
     return (
         <nav className={style.nav}>
-            <div className={style.item}><a href='#Profile'>Profile</a></div>
-            <div className={style.item}><a href='#Message'>Message</a></div>
-            <div className={style.item}><a href='#News'>News</a></div>
-            <div className={style.item}><a href='#Music'>Music</a></div>
+            <div className={style.item}><a href='/profile'>Profile</a></div>
+            <div className={style.item}><a href='/dialogs'>Message</a></div>
+            <div className={style.item}><a href='/news'>News</a></div>
+            <div className={style.item}><a href='/music'>Music</a></div>
             <br/>
-            <div className={style.item}><a href='#Settings'>Settings</a></div>
+            <div className={style.item}><a href='/settings'>Settings</a></div>
         </nav>
     )
 }
