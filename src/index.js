7fa94826a9d14bc2d642dd22c2c616e0089d16d3
Gutnik Efim <bylochka96@gmail.com>
2021-08-07 17:29:35 +0300

    add object store as global storage of data in state.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-import {renderTree} from './render'
-import state from "./redux/state";
+import store from "./redux/state";
+import ReactDOM from "react-dom";
+import {BrowserRouter} from "react-router-dom";
+import App from "./App";
 
-renderTree(state);
 
+let renderTree = () => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={store.getState()} addPost={store.createPost.bind(store)} updateTextArea={store.updateTextArea.bind(store)}/>
+        </BrowserRouter>,
+        document.getElementById('root')
+    );
+};
+
+renderTree();
+store.subscribe(renderTree);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,40 +1,46 @@
-import {renderTree} from "../render";
-
-let state = {
-    profilePage: {
-        postData: [
-            {id: 0, message: 'Hi, my name is...', likesCount: 1},
-            {id: 1, message: 'Keep silience!', likesCount: 5}
-        ],
-        newPostValue: 'samurai way'
+let store = {
+    _state: {
+        profilePage: {
+            postData: [
+                {id: 0, message: 'Hi, my name is...', likesCount: 1},
+                {id: 1, message: 'Keep silience!', likesCount: 5}
+            ],
+            newPostValue: ''
+        },
+        dialogsPage: {
+            contactsData: [
+                {id: 0, name: 'Efim'},
+                {id: 1, name: 'Vasya'},
+                {id: 2, name: 'Alina'},
+                {id: 3, name: 'Vita'},
+                {id: 4, name: 'Vlad'}
+            ],
+            messagesData: [
+                {id: 0, message: 'LOL its work!'},
+                {id: 0, message: 'Samurai way'},
+                {id: 0, message: 'How are u doing?'}
+            ]
+        }
     },
-    dialogsPage: {
-        contactsData: [
-            {id: 0, name: 'Efim'},
-            {id: 1, name: 'Vasya'},
-            {id: 2, name: 'Alina'},
-            {id: 3, name: 'Vita'},
-            {id: 4, name: 'Vlad'}
-        ],
-        messagesData: [
-            {id: 0, message: 'LOL its work!'},
-            {id: 0, message: 'Samurai way'},
-            {id: 0, message: 'How are u doing?'}
-        ]
+    getState() {
+        return this._state
+    },
+    _callSubscriber() {
+    },
+    createPost() {
+        let newPost = {id: 777, message: this._state.profilePage.newPostValue, likesCount: 777};
+        this._state.profilePage.postData.push(newPost);
+        this._state.profilePage.newPostValue = '';
+        this._callSubscriber(this._state);
+    },
+    updateTextArea(textValue) {
+        this._state.profilePage.newPostValue = textValue;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
     }
 }
 
-
-export let createPost = () => {
-    let newPost = {id: 777, message: state.profilePage.newPostValue, likesCount: 777};
-    state.profilePage.postData.push(newPost);
-    state.profilePage.newPostValue = '';
-    renderTree(state);
-}
-
-export let updateTextArea = (textValue) => {
-    state.profilePage.newPostValue = textValue;
-    renderTree(state);
-};
-
-export default state;
+window.store = store;
+export default store;
