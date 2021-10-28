ef064f799f8c12824a642bd3a574a9365c34af7c
Efim Gutnik <bylochka96@gmail.com>
2021-10-28 16:25:09 +0300

    get initial users from server with axios lib

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,67 +1,49 @@
 import React from "react";
 import style from './Users.module.css';
+import * as axios from "axios";
+import defAvatar from "../../assets/images/default-avatar.jpg";
 
 let Users = (props) => {
-
+  let getUsers = () => {
     if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                isFollowed: true,
-                avaURL: 'https://cdn1.tablethotels.com/media/ecs/global/magazine/story-images/031519-it/new/TyrionLannister.jpg',
-                name: 'Efim',
-                status: 'I am a boss',
-                location: {city: 'Minsk', country: 'Belarus'}
-            },
-            {
-                id: 2,
-                isFollowed: true,
-                avaURL: 'https://cdn1.tablethotels.com/media/ecs/global/magazine/story-images/031519-it/new/TyrionLannister.jpg',
-                name: 'Vova',
-                status: 'I am a boss',
-                location: {city: 'Moscow', country: 'Russia'}
-            },
-            {
-                id: 3,
-                isFollowed: false,
-                avaURL: 'https://cdn1.tablethotels.com/media/ecs/global/magazine/story-images/031519-it/new/TyrionLannister.jpg',
-                name: 'Astap',
-                status: 'I am a boss',
-                location: {city: 'Kiev', country: 'Ukraine'}
-            },
+      axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+        props.setUsers(response.data.items)
+      })
 
-        ])
     }
+  }
 
-    return (
-        <div>
-            {
-                props.users.map((user) => {
-                    return (
-                        <div key={user.id}>
+  return (
+    <div>
+      <button onClick={getUsers}>get Users from Server</button>
+      {
+        props.users.map((user) => {
+          return (
+            <div key={user.id}>
                         <span>
                             <div>
-                                <img src={user.avaURL} alt="avatar" className={style.userAvatar}/>
+                                <img src={user.photos.small != null ? user.avaURL : defAvatar} alt="avatar"
+                                     className={style.userAvatar}/>
                             </div>
                             <div>
                                 {user.isFollowed
-                                    ? <button onClick={() => props.toggleFollow(user.id)}>Unfollow</button>
-                                    : <button onClick={() => props.toggleFollow(user.id)}>Follow</button>}
+                                  ? <button onClick={() => props.toggleFollow(user.id)}>Unfollow</button>
+                                  : <button onClick={() => props.toggleFollow(user.id)}>Follow</button>}
                             </div>
                         </span>
-                            <span>
-                                <span><div>{user.name}</div><div>{user.status}</div></span>
+              <span>
+                                <span><div>{user.name}</div><div>{"user.status"}</div></span>
                                 <span>
-                                    <div>{user.location.country}</div>
-                                    <div>{user.location.city}</div>
+                                    <div>{"user.location.country"}</div>
+                                    <div>{"user.location.city"}</div>
                                 </span>
                             </span>
-                        </div>
-                    )
-                })
-            }
-        </div>
-    )
+            </div>
+          )
+        })
+      }
+    </div>
+  )
 }
 
 export default Users;
