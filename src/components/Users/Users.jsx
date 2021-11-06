71ca9f4f3b9fd81c0c91e8fffa3d11aca4c770c8
Efim Gutnik <bylochka96@gmail.com>
2021-11-06 14:44:11 +0300

    replaced the functional component with the Users class component, get Users in constructor

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,48 +3,50 @@ import style from './Users.module.css';
 import * as axios from "axios";
 import defAvatar from "../../assets/images/default-avatar.jpg";
 
-let Users = (props) => {
-  let getUsers = () => {
-    if (props.users.length === 0) {
+class Users extends React.Component {
+  getUsers = () => {
+    if (this.props.users.length === 0) {
       axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-        props.setUsers(response.data.items)
+        this.props.setUsers(response.data.items)
       })
 
     }
   }
 
-  return (
-    <div>
-      <button className={style.getUsers} onClick={getUsers}>get Users from Server</button>
+  render() {
+    return (
+      <div>
+        <button className={style.getUsers} onClick={this.getUsers}>get Users from Server</button>
 
-      {
-        props.users.map((user) => {
-          return (
-            <div className={style.user} key={user.id}>
+        {
+          this.props.users.map((user) => {
+            return (
+              <div className={style.user} key={user.id}>
                         <span>
                             <div>
                                 <img src={user.photos.small != null ? user.avaURL : defAvatar} alt="avatar"
                                      className={style.userAvatar}/>
                             </div>
                             <div>
-                                {user.isFollowed
-                                  ? <button onClick={() => props.toggleFollow(user.id)}>Unfollow</button>
-                                  : <button onClick={() => props.toggleFollow(user.id)}>Follow</button>}
+                              {user.isFollowed
+                                ? <button onClick={() => this.props.toggleFollow(user.id)}>Unfollow</button>
+                                : <button onClick={() => this.props.toggleFollow(user.id)}>Follow</button>}
                             </div>
                         </span>
-              <span>
+                <span>
                                 <span><div>{user.name}</div><div>{"user.status"}</div></span>
                                 <span>
                                     <div>{"user.location.country"}</div>
                                     <div>{"user.location.city"}</div>
                                 </span>
                             </span>
-            </div>
-          )
-        })
-      }
-    </div>
-  )
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
 }
 
 export default Users;
